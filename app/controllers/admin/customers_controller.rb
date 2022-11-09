c92b24e527488e3fd5e27007c2316f952922f3c9
Fuwatakahiro <110452794+fuwatakahiro@users.noreply.github.com>
2022-11-09 09:50:18 +0000

    [update]controller

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -5,12 +5,13 @@ class Admin::CustomersController < ApplicationController
   def show
     @customer=Customer.find(params[:id])
   end
-  def edit 
+  def edit
     @customer=Customer.find(params[:id])
   end
   def update
     @customer=Customer.find(params[:id])
      @customer.update!(is_deleted: true)
+     reset_session
       flash[:notice] = "ユーザーを更新しました"
       redirect_to admin_customer_path(@customer)
   end
