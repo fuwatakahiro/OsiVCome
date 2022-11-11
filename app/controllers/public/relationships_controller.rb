432fda1a27d7bec3d51f0353f64691f580191119
Fuwatakahiro <110452794+fuwatakahiro@users.noreply.github.com>
2022-11-11 11:22:42 +0000

    [update]relationship

--- app/controllers/public/relationships_controller.rb
+++ app/controllers/public/relationships_controller.rb
@@ -3,13 +3,13 @@ class Public::RelationshipsController < ApplicationController
     @customer = Customer.find(params[:customer_id])
     # relationships.create(followed_id: @customer.id)をfollowerのメソッドをmodelに作る
     current_customer.relationships.create(followed_id: @customer.id)
-    redirect_to request.referer
+    # redirect_to request.referer
   end
   def destroy
     @customer = Customer.find(params[:customer_id])
     # relationships.find_by(followed_id: @customer.id).destroyをunfollowerのメソッドをmodelに作る
     current_customer.relationships.find_by(followed_id: @customer.id).destroy
-    redirect_to request.referer
+    # redirect_to request.referer
   end
   def followings
     @customer = Customer.find(params[:customer_id])
