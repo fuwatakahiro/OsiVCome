8e7ff5e0bb8ef4a6718f418ff5c8410860648836
Fuwatakahiro <110452794+fuwatakahiro@users.noreply.github.com>
2022-11-11 10:49:09 +0000

    [update]部分テンプレートview完成

--- app/controllers/public/relationships_controller.rb
+++ app/controllers/public/relationships_controller.rb
@@ -1,11 +1,13 @@
 class Public::RelationshipsController < ApplicationController
   def create
     @customer = Customer.find(params[:customer_id])
+    # relationships.create(followed_id: @customer.id)をfollowerのメソッドをmodelに作る
     current_customer.relationships.create(followed_id: @customer.id)
     redirect_to request.referer
   end
   def destroy
     @customer = Customer.find(params[:customer_id])
+    # relationships.find_by(followed_id: @customer.id).destroyをunfollowerのメソッドをmodelに作る
     current_customer.relationships.find_by(followed_id: @customer.id).destroy
     redirect_to request.referer
   end
