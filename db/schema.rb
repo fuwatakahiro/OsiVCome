4e7b0515fc3f87ea1952df4a7fa580a51a44b6e8
Fuwatakahiro <110452794+fuwatakahiro@users.noreply.github.com>
2022-11-17 02:24:33 +0000

    コメントに対するいいねの多い順に並べページねーションを導入した

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_11_16_105511) do
+ActiveRecord::Schema.define(version: 2022_11_17_005552) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -68,6 +68,7 @@ ActiveRecord::Schema.define(version: 2022_11_16_105511) do
     t.text "content", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "favorites_count", default: 0, null: false
   end
 
   create_table "customers", force: :cascade do |t|
