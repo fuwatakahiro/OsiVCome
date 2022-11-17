901d92afa98b81435ae1c252faf6f0cd115df56b
Fuwatakahiro <110452794+fuwatakahiro@users.noreply.github.com>
2022-11-17 05:16:12 +0000

    [update]問い合わせ

--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,10 @@ Rails.application.routes.draw do
   end
 
    scope module: :public do
+     resources :contacts, only: [:new, :create]
+     post 'contacts/confirm', to: 'contacts#confirm', as: 'confirm'
+     post 'contacts/back', to: 'contacts#back', as: 'back'
+     get 'done', to: 'contacts#done', as: 'done'
      resources :characters, only: [:index, :show] do
        resources :comments, only:[:create, :destroy] do
          resource :favorites, only:[:create, :destroy]
