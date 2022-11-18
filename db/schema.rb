770027546a8ee3113c4278b9b8fc131f06b08ca4
Fuwatakahiro <110452794+fuwatakahiro@users.noreply.github.com>
2022-11-18 11:32:38 +0000

    genre

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_11_17_023258) do
+ActiveRecord::Schema.define(version: 2022_11_18_084005) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -55,7 +55,7 @@ ActiveRecord::Schema.define(version: 2022_11_17_023258) do
   create_table "characters", force: :cascade do |t|
     t.string "name", null: false
     t.string "greeting", null: false
-    t.string "genre", null: false
+    t.integer "genre_id", null: false
     t.text "reference_destination", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
@@ -97,6 +97,19 @@ ActiveRecord::Schema.define(version: 2022_11_17_023258) do
   create_table "favorites", force: :cascade do |t|
     t.integer "customer_id", null: false
     t.integer "comment_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "genres", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "notifications", force: :cascade do |t|
+    t.integer "contact_id"
+    t.boolean "checked", default: false, null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
